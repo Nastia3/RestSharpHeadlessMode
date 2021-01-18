70261acc651220333aa50bfb78a25f7c3a5626cb
Nastia3 <linka0393@gmail.com>
2021-01-18 15:08:25 +0200

    add code

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,46 @@
+﻿
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    [TestFixture]
+    public class Class1
+    {
+        IWebDriver driver;
+        [SetUp]
+        public void beforeTest()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--headless");
+            driver = new ChromeDriver(options);
+            driver.Url = "https://www.sololearn.com/";
+
+
+            //driver = new ChromeDriver("C:\\webDriver");
+            //driver.Url = "https://www.sololearn.com/";
+            //driver.Manage().Window.Maximize();
+        }
+
+        [TearDown]
+        public void afterTest()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        public void test1()
+        {
+            IWebElement button = driver.FindElement(By.XPath("//*[@id='homeHeader-v2']/div/div[1]/div/div/a[1]"));
+            button.Click();
+
+            Assert.AreEqual("https://www.sololearn.com/onboarding/0", driver.Url);
+        }
+    }
+}
